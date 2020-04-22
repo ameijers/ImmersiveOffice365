1e9b4e928925997c5b009de3392467890a3914ea
Alexander Meijers <alexander@appzinside.com>
2020-04-22 15:37:58 +0200

    Add hand menu

---
+++ ImmersiveOffice365/Assets/Scripts/Controllers/VisualController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualController : MonoBehaviour
+{
+    public void ToggleActive()
+    {
+        gameObject.SetActive(!isActiveAndEnabled);
+    }
+}
---
+++ ImmersiveOffice365/Assets/Scripts/SpatialAwareness.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialAwareness : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Use CoreServices to quickly get access to the IMixedRealitySpatialAwarenessSystem
+        var spatialAwarenessService = CoreServices.SpatialAwarenessSystem;
+
+        // Cast to the IMixedRealityDataProviderAccess to get access to the data providers
+        var dataProviderAccess = spatialAwarenessService as IMixedRealityDataProviderAccess;
+
+        var meshObserver = dataProviderAccess.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+
+        meshObserver.Disable();
+    }
+}
