b8b1f81f56d5c414d65ad59e31c7489f6e9c6267
Alexander Meijers <alexander@appzinside.com>
2020-06-21 21:45:19 +0200

    Chapter 6 distilled

---
+++ Unity/Chapter 6/ImmersiveOffice365/Assets/App/Scripts/AnimateForward.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateForward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            StartCoroutine("MoveForward");
+        }
+    }
+
+    //protected void MoveForward()
+    //{
+    //    Vector3 initialPos = gameObject.transform.position;
+
+    //    for(float posZ = 0; posZ <= 1f; posZ += 0.01f)
+    //    {
+    //        gameObject.transform.position = initialPos + new Vector3(0, 0, posZ);
+    //    }
+    //}
+
+    protected IEnumerator MoveForward()
+    {
+        Vector3 initialPos = gameObject.transform.position;
+
+        for (float posZ = 0; posZ <= 1f; posZ += 0.01f)
+        {
+            gameObject.transform.position = initialPos + new Vector3(0, 0, posZ);
+
+            yield return new WaitForSecondsRealtime(.1f);
+        }
+    }
+}
---
+++ Unity/Chapter 6/ImmersiveOffice365/Assets/App/Scripts/HandTracking.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour, IMixedRealitySourceStateHandler 
+{
+    public void OnSourceDetected(SourceStateEventData eventData)
+    {
+        IMixedRealityHand hand = eventData.Controller as IMixedRealityHand;
+
+        if (hand != null)
+        {
+            if (hand.TryGetJoint(TrackedHandJoint.Wrist, out MixedRealityPose pose))
+            {
+                Vector3 positionOfWrist = pose.Position;
+            }
+        }
+    }
+
+    public void OnSourceLost(SourceStateEventData eventData)
+    {
+    }
+
+    public void Update()
+    {
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, Handedness.Any, out MixedRealityPose pose))
+        {
+            Vector3 positionOfWrist = pose.Position;
+            IMixedRealityHand whichHand = HandJointUtils.FindHand(Handedness.Any);
+        }
+    }
+}
